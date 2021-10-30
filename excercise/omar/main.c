512b3c5825ee9daa15d70b09aa81aa5863590a68
omaratif1995 <52365335+omaratif1995@users.noreply.github.com>
2021-10-30 12:37:38 +0200

    updated script

--- excercise/omar/main.c
+++ excercise/omar/main.c
@@ -3,43 +3,56 @@
 #include <stdint.h>
 #include <pthread.h>
 
-int x = 0;
-pthread_mutex_t test_mutex;
+int counter = 0;
+pthread_mutex_t test_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *func1(void* tid)
 {
     long * myID = (long *)tid;
 
 
+//    do{
+//
+//
+//        //Lock mutex before going to change variable
+//        pthread_mutex_lock(&test_mutex);
+//        printf("Hello i am thread number %d\n",*myID);
+//        x++;
+//        printf("x = %d\n",x);
+//
+//        //Unlock mutex after changing the variable
+//        pthread_mutex_unlock(&test_mutex);
+//    }while(x < 99);
 
     for (int i = 0; i < 100; i++ )
     {
         pthread_mutex_lock(&test_mutex);
-        if(x < 100){
+        if(counter < 100){
         //Lock mutex before going to change variable
 
-        printf("Hello i am thread number %d\n",*myID);
-        x++;
-        printf("x = %d\n",x);
+        printf("Hello i am the gate number %d\n",*myID);
+        counter++;
+        printf("counter = %d\n",counter);
 
+
+        }
         //Unlock mutex after changing the variable
         pthread_mutex_unlock(&test_mutex);
-        }
-
     }
+
 }
 
 int main()
 {
-    pthread_t tid0;
-    pthread_t tid1;
-    pthread_t * pthreads[] = {&tid0,&tid1};
+    pthread_t right;
+    pthread_t left;
+    pthread_t * pthreads[] = {&right,&left};
     for(int i = 0;i<2;i++){
         pthread_create(pthreads[i], NULL, func1, (void *)pthreads[i]);
     }
 
-    pthread_join(tid0,NULL);
-    pthread_join(tid1,NULL);
+    pthread_join(right,NULL);
+    pthread_join(left,NULL);
     return 0;
 }
 
