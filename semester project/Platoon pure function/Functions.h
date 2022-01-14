ff26fecb8f178a8636b54eeab24731811c94c157
Ibrahim Ahmed <ibrahim.ahmed003@stud.fh-dortmund.de>
2022-01-14 20:11:21 +0100

    Slight changes to the Platoon Pure Function Code: Removing time delays

--- semester project/Platoon pure function/Functions.h
+++ semester project/Platoon pure function/Functions.h
@@ -10,17 +10,14 @@ void thread_maintainConnection() {
 #pragma omp task
 {
 	if (!truck1.isConnected) truck0.connect(truck1);
-	std::this_thread::sleep_for(100ms);
 }		
 #pragma omp task
 {
 	if (!truck2.isConnected) truck0.connect(truck2);
-	std::this_thread::sleep_for(100ms);
 }
 #pragma omp task
 {
 	if (!truck3.isConnected) truck0.connect(truck3);
-	std::this_thread::sleep_for(100ms);
 }
 }	
 	}
@@ -36,7 +33,6 @@ void thread_speedControl() {
 	double v_current = truck1.get_velocity();
 	if (v_current < v_target) { truck1.accelerate(v_target); }
 	else if (v_current > v_target) { truck1.deccelerate(v_target); }
-	std::this_thread::sleep_for(200ms);
 }
 #pragma omp task
 {
@@ -44,7 +40,6 @@ void thread_speedControl() {
 	double v_current = truck2.get_velocity();
 	if (v_current < v_target) { truck2.accelerate(v_target); }
 	else if (v_current > v_target) { truck2.deccelerate(v_target); }
-	std::this_thread::sleep_for(200ms);
 }
 #pragma omp task
 {
@@ -52,7 +47,6 @@ void thread_speedControl() {
 	double v_current = truck3.get_velocity();
 	if (v_current < v_target) { truck3.accelerate(v_target); }
 	else if (v_current > v_target) { truck3.deccelerate(v_target); }
-	std::this_thread::sleep_for(200ms);
 }
 }
 	}
@@ -66,19 +60,16 @@ void thread_steering() {
 {
 	double target_angle = truck0.get_angle();
 	truck1.set_angle(target_angle);
-	std::this_thread::sleep_for(100ms);
 }
 #pragma omp task
 {
 	double target_angle = truck0.get_angle();
 	truck2.set_angle(target_angle);
-	std::this_thread::sleep_for(100ms);
 }
 #pragma omp task
 {
 	double target_angle = truck0.get_angle();
 	truck3.set_angle(target_angle);
-	std::this_thread::sleep_for(100ms);
 }
 }
 	}
@@ -91,17 +82,14 @@ void thread_braking() {
 #pragma omp task
 {
 	if (truck0.isBraking) truck1.brake();
-	std::this_thread::sleep_for(100ms);
 }
 #pragma omp task
 {
 	if (truck0.isBraking) truck2.brake();
-	std::this_thread::sleep_for(100ms);
 }
 #pragma omp task
 {
 	if (truck0.isBraking) truck3.brake();
-	std::this_thread::sleep_for(100ms);	
 }
 }
 	}
@@ -114,17 +102,14 @@ void thread_maintainDistance() {
 #pragma omp task
 {
 	truck1.maintain_distance();
-	std::this_thread::sleep_for(200ms);
 }
 #pragma omp task
 {
 	truck1.maintain_distance();
-	std::this_thread::sleep_for(200ms);
 }
 #pragma omp task
 {
 	truck1.maintain_distance();
-	std::this_thread::sleep_for(200ms);
 }
 }
 	}
